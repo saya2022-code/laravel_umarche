23488a927b252e000612b81c4f933334cb731573
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-06-13 16:44:01 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+</div>
